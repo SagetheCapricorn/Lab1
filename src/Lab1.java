6dc2597ffb022c782625aadee8d1d41ee48b3594
BT_1N3_19@BKTECH.LOCAL <BT_1N3_19@BKTECH.LOCAL>
2018-10-10 14:50:37 -0400

    Initial commit

---
+++ src/Lab1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Lab1 {
+
+    public static void main(String [] args){
+        Scanner we=new Scanner(System.in);
+        String name= we.nextLine();
+        System.out.println("Hiya"+ name+ "! NIce to meet you!");
+        int x= (int)(Math.random()*101);
+        Scanner in= new Scanner(System.in);
+        System.out.println("Guess my hidden number!!");
+        int y = in.nextInt();
+        while(y != x){
+            System.out.println("Sorry "+name+ ":(. Please Guess Again!");
+            y = in.nextInt();
+        }
+        System.out.println("Congrats you're a great guesser!!!");
+    }
+}
