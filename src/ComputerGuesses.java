57079e922526c5b5d49d57342cdc2caa4469bcaa
SagetheCapricorn <oswitoryio12>
2018-10-11 14:48:29 -0400

    Needs work

---
+++ src/ComputerGuesses.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ComputerGuesses {
+
+        public static void main(String[] args) {
+            System.out.println("Hi there!! Whats your name?");
+            Scanner we = new Scanner(System.in);
+            String name = we.nextLine();
+            System.out.println("Hiya " + name + "! Nice to meet you! Lets play the guessing game");
+            int x = (int) (Math.random() * 51);
+            System.out.println("Is your number " + x + " ?");
+            Scanner in = new Scanner(System.in);
+            String y = in.nextLine();
+            while (!y.equals("yes")) {
+                if (y.equals("higher")) {
+                    x = (int) (Math.random() * (51 + 5));
+                    System.out.println("Is your number" + x + "?");
+                    String y = in.nextLine();
+                }
+                if (y.equals ("lower")) {
+                        x = (int) (Math.random() * (51 - 5));
+                        System.out.println("Is your number" + x + "?");
+                    String y = in.nextLine();
+
+                }
+
+
+            }
+            if (y.equals ("yes")){
+                System.out.println("They call me the best guesser around!!");
+            }
+
+            }
+        }
--- src/Lab1.java
+++ src/Lab1.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Lab1 {
 
     public static void main(String [] args){
+        System.out.println("Hi there!! Whats your name?");
         Scanner we=new Scanner(System.in);
         String name= we.nextLine();
-        System.out.println("Hiya"+ name+ "! NIce to meet you!");
+        System.out.println("Hiya "+ name+ "! Nice to meet you!");
         int x= (int)(Math.random()*101);
         Scanner in= new Scanner(System.in);
         System.out.println("Guess my hidden number!!");
         int y = in.nextInt();
         while(y != x){
-            System.out.println("Sorry "+name+ ":(. Please Guess Again!");
+            System.out.println("Sorry "+name+ ":( Please Guess Again!");
             y = in.nextInt();
         }
         System.out.println("Congrats you're a great guesser!!!");
